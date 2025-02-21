e42c4a9c8aadc235612b61e7e206b3f1f191970d
Mohmed Shawky <57459035+mohamedshawky70@users.noreply.github.com>
2025-02-21 02:38:26 +0200

    Update Controller.cs

--- Controller.cs
+++ Controller.cs
@@ -1,26 +1,27 @@
 //Inject                      shape that your data return in it
 private readonly ICashService<IEnumerable<QuestionResponse>> _cashService;
+private const string cashPrefix = "allQuestion";
 
-		public async Task<IActionResult> GetAllAsync([FromRoute] int PollId, CancellationToken cancellationToken)
-		{
-			var poll = await _unitOfWork.polls.GetByIdAsync(PollId);
-			if (poll is null)
-				return NotFound(PollErrors.NotFound);
+public async Task<IActionResult> GetAllAsync([FromRoute] int PollId, CancellationToken cancellationToken)
+{
+	var poll = await _unitOfWork.polls.GetByIdAsync(PollId);
+	if (poll is null)
+		return NotFound(PollErrors.NotFound);
 
-			//مفتاح هعمل الكاش بيه وهحذفه وهجيبه بيه _
-			//يونيك فاليو علشان ميحفظش كل الاسأله بكل البولز بتاعتها بنفس المفتاح علشان لما تجيب الاسأله بتاعة بول واحد يجيب بتاعة بول واحد بس
-			var cachKey = $"{cashPrefix}-{PollId}";
-			var cachedQuestion = await _cashService.GetAsync(cachKey, cancellationToken);
-			IEnumerable<QuestionResponse> response = [];
-			if (cachedQuestion is null)
-			{
-				var questions = await _unitOfWork.questions.FindAllInclude(x => x.PollId == PollId, cancellationToken, new[] { "answers" });
-				response = questions.Adapt<IEnumerable<QuestionResponse>>();
-				await _cashService.SetAsync(cachKey, response, cancellationToken);
-			}
-			else
-			{
-				response = cachedQuestion;
-			}
-			return Ok(response);
-		}
+	//مفتاح هعمل الكاش بيه وهحذفه وهجيبه بيه _
+	//يونيك فاليو علشان ميحفظش كل الاسأله بكل البولز بتاعتها بنفس المفتاح علشان لما تجيب الاسأله بتاعة بول واحد يجيب بتاعة بول واحد بس
+	var cachKey = $"{cashPrefix}-{PollId}";
+	var cachedQuestion = await _cashService.GetAsync(cachKey, cancellationToken);
+	IEnumerable<QuestionResponse> response = [];
+	if (cachedQuestion is null)
+	{
+		var questions = await _unitOfWork.questions.FindAllInclude(x => x.PollId == PollId, cancellationToken, new[] { "answers" });
+		response = questions.Adapt<IEnumerable<QuestionResponse>>();
+		await _cashService.SetAsync(cachKey, response, cancellationToken);
+	}
+	else
+	{
+		response = cachedQuestion;
+	}
+	return Ok(response);
+}
